b7c3bb1c0834017bbd0f27ef322803fc8856407e
Sim <simyujie@gmail.com>
2020-05-29 18:32:35 +0800

    added fb login

--- app/src/main/java/orbital/gns/tpspacefinder/Activities/LoginActivity.java
+++ app/src/main/java/orbital/gns/tpspacefinder/Activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package orbital.gns.tpspacefinder.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,9 +13,20 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,16 +35,39 @@ import orbital.gns.tpspacefinder.R;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private CallbackManager mCallbackManager;
+    private LoginButton facebookLoginButton;
+    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
-//        The button to go back to the starting page
+        facebookLoginButton = findViewById(R.id.facebookLoginButton);
+        facebookLoginButton.setReadPermissions("email", "public_profile");
+        mCallbackManager = CallbackManager.Factory.create();
+        facebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("debug", "yay");
+                handleFacebookToken(loginResult.getAccessToken());
+            }
 
-//        The button to login into an account
+            @Override
+            public void onCancel() {
+                Log.d("debug", "wtf1");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("debug", "wtf");
+            }
+        });
+
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +75,54 @@ public class LoginActivity extends AppCompatActivity {
                 getLoginDetails();
             }
         });
+
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LoginActivity.super.onBackPressed();
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                } else {
+                  Log.d("debug", "fak");
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    mAuth.signOut();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    changeActivity();
+                } else {
+                    Log.d("debug", "Something went wrong");
+                }
             }
         });
     }
@@ -78,11 +156,7 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d("debug", "Successful Login");
-                            Intent intent = new Intent(getApplicationContext(), ActionPageActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(intent);
-                            LoginActivity.super.finish();
+                            changeActivity();
                         } else {
                             Log.d("debug", "Wrong email or password");
                             Toast.makeText(getApplicationContext(), "Wrong email or password", Toast.LENGTH_SHORT).show();
@@ -91,6 +165,26 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
+    private void changeActivity() {
+        Log.d("debug", "Successful Login");
+        Intent intent = new Intent(getApplicationContext(), ActionPageActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        LoginActivity.super.finish();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
 }
 
